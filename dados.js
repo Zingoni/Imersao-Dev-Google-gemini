8c5c0ae4c3845c734006a4bb09c887f1026503c1
Zingoni <103472838+Zingoni@users.noreply.github.com>
2024-09-08 04:34:43 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,44 @@
+let dados = [
+    {
+      titulo: "Demon Slayer",
+      descricao: "Demon Slayer, ou Kimetsu no Yaiba em japonês, é um anime que conquistou o coração de milhões de fãs ao redor do mundo. A história gira em torno de Tanjiro Kamado, um jovem carvoeiro que tem sua vida drasticamente alterada após um ataque demoníaco que massacra sua família.",
+      link: "https://pt.wikipedia.org/wiki/Kimetsu_no_Yaiba",
+      tags: "demonio demon espadachin hashira"
+    },
+    {
+      titulo: "My Hero Academia",
+      descricao: "My Hero Academia é um anime que se passa em um mundo onde a maioria das pessoas possuem superpoderes, chamados de Quirks. A história acompanha Izuku Midoriya, um garoto que nasceu sem poderes, mas que sonha em se tornar um herói.",
+      link: "https://pt.wikipedia.org/wiki/Boku_no_Hero_Academia",
+      tags: "menino heroi escola"
+    },
+    {
+      titulo: "Naruto",
+      descricao: "Naruto é um anime que conta a história de um jovem ninja chamado Naruto Uzumaki que busca reconhecimento e sonha em se tornar o Hokage, o líder da vila da folha.",
+      link: "https://pt.wikipedia.org/wiki/Naruto",
+      tags: "kiubi konoha vila"
+    },
+    {
+      titulo: "Solo Leveling",
+      descricao: "Um RPG online se torna uma realidade mortal quando portões para outro mundo se abrem. Sung Jin-woo, o mais fraco dos caçadores, encontra uma maneira de subir de nível sozinho e se torna o mais poderoso do mundo.",
+      link: "https://myanimelist.net/anime/46954/Solo_Leveling",
+      tags: "ação fantasia RPG sobrenatural"
+    },
+    {
+      titulo: "Kaiju No. 8",
+      descricao: "Em um mundo onde gigantescas criaturas conhecidas como Kaiju ameaçam a humanidade, Kafka Hibino, um homem comum, se transforma em um poderoso Kaiju para proteger a cidade que ama.",
+      link: "https://myanimelist.net/anime/51226/Kaiju_No_8",
+      tags: "ação shonen kaiju sci-fi"
+    },
+    {
+      titulo: "Chainsaw Man",
+      descricao: "Denji, um jovem pobre que vive com um demônio de motosserra chamado Pochita, faz um pacto para se tornar um caçador de demônios e obter uma vida melhor.",
+      link: "https://myanimelist.json/anime/45473/Chainsaw_Man",
+      tags: "ação shonen sobrenatural horror"
+    },
+    {
+      titulo: "Spy x Family",
+      descricao: "Uma comédia de espionagem que acompanha Loid Forger, um espião que precisa construir uma família falsa para completar uma missão. Mas sua esposa é uma assassina e sua filha uma telepata.",
+      link: "https://myanimelist.net/anime/45632/Spy_Family",
+      tags: "comédia espionagem família"
+    }
+  ];
